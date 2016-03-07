578c673819629fa2b58b0d1fdb43b70e28b6a6f1
Rafael Ivan Garcia Escobedo <Ivan@Ivan>
2016-03-07 00:02:06 -0600

    Hello World With node js.

---
+++ PROTEUS/main.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+       'Content-Type' : 'text/plain; charset=UTF-8' 
+    });
+    
+    res.end("Hello World. test");
+    
+    
+}).listen(9080, "");
